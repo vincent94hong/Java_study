4610ab679481ede48ca6d022596ed3637ccbf618
vincent94hong@gmail.com <you@example.com>
2022-11-24 14:49:00 +0900

    221124_1448

--- Java_study/src/ch03_test_1018/Hello.java
+++ Java_study/src/ch03_test_1018/Hello.java
@@ -4,17 +4,16 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 실행은 ctrl + f11
-		// 주석이 보통은 ctrl + / : 단일 행 주석.
-		// 한번 더 ctrl + / : 취소가 됨.
+		// 실행은 cmd + f11
+		// 주석이 보통은 cmd + / : 단일 행 주석.
+		// 한번 더 cmd + / : 취소가 됨.
 
-		// 여러 행 주석 : ctrl + shift + /
-		// 여러 행 주석 해제 : ctrl + shift + \
-		// 자동 포맷 정렬 : ctrl + shift + f 
-		System.out.println("Hello 이상용 자바");
-		System.out.println("Hello 이상용 자바");
+		// 여러 행 주석 : cmd + shift + /
+		// 여러 행 주석 해제 : cmd + shift + \
+		// 자동 포맷 정렬 : cmd + shift + f
+		System.out.println("Hello vincent java");
+//		sysout
 
-		System.out.println("Hello 이상용 자바");
 	}
 
 }
