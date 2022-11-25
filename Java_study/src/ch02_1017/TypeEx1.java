00ac4f69d2beb1c96655cc241568fc4d58595774
vincent94hong@gmail.com <you@example.com>
2022-11-25 11:17:15 +0900

    221125_1117

--- Java_study/src/ch02_1017/TypeEx1.java
+++ Java_study/src/ch02_1017/TypeEx1.java
@@ -1,18 +1,26 @@
 package ch02_1017;
 
 public class TypeEx1 {
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
+		byte b1 = 3; // 초기화 : 최초에 값을 할당, 대입한다.
+		int i1 = 200;
+		byte b2 = (byte) i1; // ( ) 소괄호의 의미는 캐스팅 연산자라고 함.
+		// 강제로 해당 소괄호 안에 형으로 명시적, 강제적 형변환.
+		// 운좋게 해당 값이 담을 수 있다면, 정상 출력 되겠지만, 담을 수 있는 범위를 넘어가는데도 강제로 형변환을 하면 의미없는 값이 나온다. >
+		// 값 손실이 발생.
+		System.out.println(" b2 출력-강제로 형변환-값의 범위초과 : " + b2);
+
 		int i = 10;
 		var j = 10.0;
 		var str = "hello";
-		
+
 		System.out.println(i);
 		System.out.println(j);
 		System.out.println(str);
-		
+
 //		str = "test";
-		
+
 //		str = 3; //오류 남  
 	}
 
--- Java_study/src/ch03_test_1018/OperationEx3.java
+++ Java_study/src/ch03_test_1018/OperationEx3.java
@@ -7,13 +7,13 @@ public class OperationEx3 {
 		int i = 2;
 		
 		boolean value = ((num1 = num1 + 10 ) < 10) && ( ( i = i + 2 ) < 10);
-		System.out.println(value);
-		System.out.println(num1);
-		System.out.println(i);
+		System.out.println("value 의 값" + value);
+		System.out.println("num1 의 값" + num1);
+		System.out.println("i 의 값" + i);
 		
 		value = ((num1 = num1 + 10 ) > 10) || ( ( i = i + 2 ) < 10);
-		System.out.println(value);
-		System.out.println(num1);
-		System.out.println(i);
+		System.out.println("value 의 값" + value);
+		System.out.println("num1 의 값" + num1);
+		System.out.println("i 의 값" + i);
 	}
 }
